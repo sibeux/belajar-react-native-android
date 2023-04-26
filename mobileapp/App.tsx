c0a182f7ffbefd8d5735432a2c2a684213725eea
Nasrul Wahabi <wahabinasrul@gmail.com>
2023-04-26 10:58:55 +0700

    view

--- mobileapp/App.tsx
+++ mobileapp/App.tsx
@@ -8,7 +8,7 @@ class App extends Component {
   }
   render() {
     return (
-      <View>
+      <View style={{flex: 1}}>
         <Text
           style={{
             fontSize: 30,
@@ -17,15 +17,37 @@ class App extends Component {
             textAlign: 'center',
             letterSpacing: 2,
           }}>
-          {' '}
           <Text
             style={{color: 'rgb(255,0,0)', textDecorationLine: 'line-through'}}>
             Hello
           </Text>
           <Text style={{color: 'blue', textDecorationLine: 'underline'}}>
             World
-          </Text>{' '}
+          </Text>
         </Text>
+
+        <View
+          style={{
+            // width: 50,
+            // height: 50,
+            flex: 0.5,
+            backgroundColor: 'cyan',
+            // marginLeft: 30,
+            // paddingTop: 5,
+            marginHorizontal: 20,
+            marginVertical: 30,
+            // paddingLeft: 10,
+            justifyContent: 'center',
+            alignItems: 'center',
+
+            flexDirection: 'column-reverse',
+            // flexDirection: 'row',
+          }}>
+          <Text>{'sibe 1'}</Text>
+          <Text>{'sibe 2'}</Text>
+          <Text>{'sibe 3'}</Text>
+          <Text>{'sibe 4'}</Text>
+        </View>
       </View>
     );
   }
