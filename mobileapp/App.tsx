849d78bf7ad14e7c32d4e0f0f221629a3e67d4ad
Nasrul Wahabi <wahabinasrul@gmail.com>
2023-04-26 09:05:59 +0700

    text style

--- mobileapp/App.tsx
+++ mobileapp/App.tsx
@@ -1,15 +1,27 @@
-import React, { Component } from 'react';
-import { Text, View } from 'react-native/types';
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
 
 class App extends Component {
   constructor(props: {} | Readonly<{}>) {
     super(props);
-    this.state = {  };
+    this.state = {};
   }
   render() {
     return (
       <View>
-        <Text> Hello World </Text>
+        <Text
+          style={{
+            color: 'rgb(255,0,0)',
+            fontSize: 30,
+            fontWeight: 'bold',
+            fontStyle: 'italic',
+            textAlign: 'center',
+            textDecorationLine: 'underline line-through',
+            letterSpacing: 2
+          }}>
+          {' '}
+          Hello World{' '}
+        </Text>
       </View>
     );
   }
