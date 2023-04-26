094f4262452ea0ef7f76db9ce7741cf2e94f4835
Nasrul Wahabi <wahabinasrul@gmail.com>
2023-04-27 01:49:41 +0700

    scroll view, switch, textinput

--- mobileapp/App.tsx
+++ mobileapp/App.tsx
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
-import { Text, View, StatusBar, Image, TouchableOpacity } from 'react-native';
+import {
+  Text,
+  View,
+  StatusBar,
+  Image,
+  TouchableOpacity,
+  Switch,
+  TextInput,
+  ScrollView,
+} from 'react-native';
+
 import Basic from './Udemy/Basic';
 
-class App extends Component<{}, { header: string }> {
+class App extends Component<
+  {},
+  { header: string; value: boolean; username: string }
+> {
   constructor(props: any) {
     super(props);
     this.state = {
       header: 'Shopeepay',
+      value: true,
+      username: '',
     };
   }
   render() {
     return (
-      <View style={{ flex: 1 }}>
+      <ScrollView style={{ flex: 1 }}>
         <StatusBar backgroundColor="#f57c00" barStyle="light-content" />
         <View
           style={{
@@ -43,7 +58,7 @@ class App extends Component<{}, { header: string }> {
             resizeMode="contain"
           />
 
-          <View style={{ width: 20, backgroundColor: '#000' }} />
+          <View style={{ width: 20 }} />
 
           <View>
             <Image
@@ -54,6 +69,20 @@ class App extends Component<{}, { header: string }> {
             />
           </View>
         </View>
+
+        <TextInput
+          style={{
+            borderWidth: 1,
+            marginHorizontal: 20,
+            marginTop: 20,
+            paddingHorizontal: 5,
+            borderRadius: 5,
+            borderColor: '#f57c00',
+            backgroundColor: 'white',
+          }}
+          value={this.state.username}
+          onChangeText={value => this.setState({ username: value })}
+        />
 
         <TouchableOpacity
           style={{
@@ -68,12 +97,53 @@ class App extends Component<{}, { header: string }> {
             marginLeft: 'auto',
             marginRight: 'auto',
           }}
-          onPress={() => console.log('hello')}>
+          onPress={() => console.log(this.state.username)}>
           <Text style={{ color: 'white', fontWeight: 'bold' }}>
             Teks ini bisa diklik
           </Text>
         </TouchableOpacity>
-      </View>
+
+        <View
+          style={{
+            marginTop: 10,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Switch
+            value={this.state.value}
+            onChange={() => this.setState({ value: !this.state.value })}
+          />
+        </View>
+
+        <View
+          style={{
+            marginTop: 20,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Image
+            style={{
+              width: 300,
+              height: 300,
+              backgroundColor: '#f57c00',
+            }}
+            // image offline
+            source={require('./src/image/image1.jpeg')}
+            resizeMode="cover" // default
+          />
+
+          <Image
+            style={{
+              width: 300,
+              height: 300,
+              backgroundColor: '#f57c00',
+            }}
+            // image offline
+            source={require('./src/image/image1.jpeg')}
+            resizeMode="cover" // default
+          />
+        </View>
+      </ScrollView>
     );
   }
 }
